d9a04300cf0e3660649c6954056ae144839be281
QuinnStahlman <113214156+QuinnStahlman@users.noreply.github.com>
2022-09-16 10:31:58 -0700

    set up camera movement

--- CreateWithCode/Prototype 1/Assets/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/PlayerController.cs
@@ -17,6 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
       //Move the vehicle forward
-      transform.Translate(Vector3.forward * Time.deltaTime * 20);
+      transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
