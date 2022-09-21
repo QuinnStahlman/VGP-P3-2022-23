9c63acd22bd8cec3429b573c19924c8eb44a32bb
QuinnStahlman <113214156+QuinnStahlman@users.noreply.github.com>
2022-09-21 10:17:32 -0700

    Organized, added turn, made private
    Made variables private, added rotation on turn, organized hierarchy

--- CreateWithCode/Prototype 1/Assets/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed=5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
 
     // Start is called before the first frame update
@@ -23,8 +23,9 @@ public class PlayerController : MonoBehaviour
       forwardInput = Input.GetAxis("Vertical");
 
 
-      //Move the vehicle forward
+      //Move the vehicle forward on vertical input
       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-      transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+      //Turns the vehicle based on horizontal input
+      transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
