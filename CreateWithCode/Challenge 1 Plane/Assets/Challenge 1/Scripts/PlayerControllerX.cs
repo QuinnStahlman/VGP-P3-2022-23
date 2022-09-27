1e805fc24df68482148e8f1cdcfcbc3694775fd1
QuinnStahlman <113214156+QuinnStahlman@users.noreply.github.com>
2022-09-27 10:32:30 -0700

    new plane controls

--- CreateWithCode/Challenge 1 Plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ CreateWithCode/Challenge 1 Plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float rotationSpeed = 2000.0f;
+    private float speed = 40.0f;
+    private float rotationSpeed = 80.0f;
     private float verticalInput;
     private float forwardInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +20,15 @@ public class PlayerControllerX : MonoBehaviour
     void FixedUpdate()
     {
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        forwardInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
 
         // tilt the plane up/down based on up/down arrow keys
         
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed * verticalInput);
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
     }
 }
