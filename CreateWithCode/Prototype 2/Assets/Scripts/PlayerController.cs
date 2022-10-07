b1bc86ab50b63f45ad9272c244bc6c6822e1e343
QuinnStahlman <113214156+QuinnStahlman@users.noreply.github.com>
2022-10-07 09:54:25 -0700

    new key input

--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,9 @@ public class PlayerController : MonoBehaviour
        }
        if( transform.position.x > xRange) {
         transform.position = new Vector3 (xRange, transform.position.y, transform.position.z);
+       }
+       if (Input.GetKeyDown(KeyCode.Space))
+       {//Launch projectile from player
        }
        horizontalInput = Input.GetAxis("Horizontal"); 
        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
